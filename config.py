205e66ea8f893a8e4ccefd3060576d05bd8eddcb
suk0ng <wlsaud031206@gmail.com>
2025-07-27 22:02:06 +0900

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--modeldir', help='Folder containing .tflite and labelmap', required=True)
+    parser.add_argument('--threshold', help='Minimum confidence threshold', default=0.5)
+    parser.add_argument('--resolution', help='Camera resolution WxH', default='1280x720')
+    args = parser.parse_args()
+
+    MODEL_NAME = args.modeldir
+    GRAPH_NAME = 'detect.tflite'
+    LABELMAP_NAME = 'labelmap.txt'
+    min_conf_threshold = float(args.threshold)
+    res_w, res_h = map(int, args.resolution.split('x'))
+
+    return MODEL_NAME, GRAPH_NAME, LABELMAP_NAME, min_conf_threshold, res_w, res_h
