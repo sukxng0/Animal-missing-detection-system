3eee8050c93f89829abbcfeb4816d34b1ca5d989
suk0ng <wlsaud031206@gmail.com>
2025-07-27 22:01:01 +0900

    Create tflite_loader.py

---
+++ tflite_loader.py
@@ -0,0 +1,21 @@
+import os
+import importlib.util
+import numpy as np
+
+def load_interpreter(model_path):
+    pkg = importlib.util.find_spec('tflite_runtime')
+    if pkg:
+        from tflite_runtime.interpreter import Interpreter
+    else:
+        from tensorflow.lite.python.interpreter import Interpreter
+
+    interpreter = Interpreter(model_path=model_path)
+    interpreter.allocate_tensors()
+
+    input_details = interpreter.get_input_details()
+    output_details = interpreter.get_output_details()
+    height = input_details[0]['shape'][1]
+    width = input_details[0]['shape'][2]
+    floating_model = input_details[0]['dtype'] == np.float32
+
+    return interpreter, input_details, output_details, height, width, floating_model
